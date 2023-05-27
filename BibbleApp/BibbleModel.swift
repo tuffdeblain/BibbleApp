2acaf3473c72d452c3bfaa4dcd561613601c2344
tuffdeblain <himanfullhd@gmail.com>
2023-05-27 17:47:08 +0700

    created main view with tableview

--- BibbleApp/BibbleModel.swift
+++ BibbleApp/BibbleModel.swift
@@ -4,12 +4,12 @@
 //
 import Foundation
 
-// MARK: - Search
+
 struct Search: Codable {
     let data: [Bibble]?
 }
 
-// MARK: - Datum
+
 struct Bibble: Codable {
     let id, dblID, abbreviation, abbreviationLocal: String?
     let language: Language?
@@ -23,20 +23,52 @@ struct Bibble: Codable {
         case dblID = "dblId"
         case abbreviation, abbreviationLocal, language, countries, name, nameLocal, description, descriptionLocal, relatedDbl, type, updatedAt, audioBibles
     }
+    
+
+    init(id: String? = nil,
+         dblID: String? = nil,
+         abbreviation: String? = nil,
+         abbreviationLocal: String? = nil,
+         language: Language? = nil,
+         countries: [Country]? = nil,
+         name: String? = nil,
+         nameLocal: String? = nil,
+         description: String? = nil,
+         descriptionLocal: String? = nil,
+         relatedDbl: String? = nil,
+         type: String? = nil,
+         updatedAt: String? = nil,
+         audioBibles: [AudioBible]? = nil) {
+        self.id = id
+        self.dblID = dblID
+        self.abbreviation = abbreviation
+        self.abbreviationLocal = abbreviationLocal
+        self.language = language
+        self.countries = countries
+        self.name = name
+        self.nameLocal = nameLocal
+        self.description = description
+        self.descriptionLocal = descriptionLocal
+        self.relatedDbl = relatedDbl
+        self.type = type
+        self.updatedAt = updatedAt
+        self.audioBibles = audioBibles
+    }
 }
 
-// MARK: - AudioBible
+
+
 struct AudioBible: Codable {
     let id, name, nameLocal, description: String?
     let descriptionLocal: String?
 }
 
-// MARK: - Country
+
 struct Country: Codable {
     let id, name, nameLocal: String?
 }
 
-// MARK: - Language
+
 struct Language: Codable {
     let name, script: String?
 }
--- BibbleApp/ViewController.swift
+++ BibbleApp/ViewController.swift
@@ -8,21 +8,31 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     let headers: HTTPHeaders = ["api-key": "b4a7bf98adb238d0a3a3b9a826060a4b"]
     var searchResult: Search?
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
         fetchBibles(url: URLS.url.rawValue, header: headers)
-
     }
 
+    func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BibleCell")
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
 
-}
-
-extension ViewController {
     func fetchBibles(url: String, header: HTTPHeaders) {
         NetworkManager.shared.request(url: url, method: .get, headers: headers) { [weak self] result in
             switch result {
@@ -32,6 +42,9 @@ extension ViewController {
                         let decoder = JSONDecoder()
                         let search = try decoder.decode(Search.self, from: data)
                         self?.searchResult = search
+                        DispatchQueue.main.async {
+                            self?.tableView.reloadData()
+                        }
                     } catch {
                         print("Decoding error: \(error)")
                     }
@@ -40,5 +53,18 @@ extension ViewController {
                 print("Error: \(error)")
             }
         }
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResult?.data?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BibleCell", for: indexPath)
+        if let bibleName = searchResult?.data?[indexPath.row].name {
+            cell.textLabel?.text = bibleName
+        }
+        return cell
     }
 }
